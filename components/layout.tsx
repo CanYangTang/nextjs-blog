752a950182aa6e5bfbd15c430db1067ebd3d2022
CanYangTang <smzdxlt@163.com>
2021-09-02 14:27:02 +0800

    add typescript

--- components/layout.tsx
+++ components/layout.tsx
@@ -2,11 +2,15 @@ import Head from 'next/head'
 import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
+import { ReactInstance } from 'react'
 
 const name = 'TangCanyang'
 export const siteTitle = 'Next.js Sample Website'
 
-export default function Layout({ children, home }) {
+export default function Layout({ children, home }: { 
+  children: React.ReactNode 
+  home?: boolean
+}) {
   return (
     <div className={styles.container}>
       <Head>
